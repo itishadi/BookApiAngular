509975407ba53c9022a3d4dd3ac14917eaf9f59f
itishadi <113441103+itishadi@users.noreply.github.com>
2025-11-25 13:55:04 +0100

    Uppdaterade navbar komponentens HTML och TS

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,23 +1,3 @@
-// // src/app/layout/navbar/navbar.component.ts
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { RouterModule } from '@angular/router';
-
-// @Component({
-//   selector: 'app-navbar',              // <-- rätt selector
-//   standalone: true,
-//   templateUrl: './navbar.component.html', // <-- pekar på sin egen HTML-fil
-//   styleUrls: ['./navbar.component.scss'], // om du har en SCSS-fil
-//   imports: [CommonModule, RouterModule]
-// })
-// export class NavbarComponent {
-//   isDark = false;
-
-//   toggleTheme() {
-//     this.isDark = !this.isDark;
-//     document.body.classList.toggle('dark', this.isDark);
-//   }
-// }
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -35,11 +15,6 @@ export class NavbarComponent {
   isDark = false;
   loggedIn = false;
 
-  // constructor(public auth: AuthService) {
-  //   // prenumerera på loginstatus
-  //   this.auth.isLoggedIn().subscribe(status => this.loggedIn = status);
-  // }
-
   toggleTheme() {
     this.isDark = !this.isDark;
     document.body.classList.toggle('dark', this.isDark);
@@ -52,8 +27,4 @@ logout() {
   this.auth.logout();
   this.router.navigate(['/login']); // skickar användaren till login-sidan
 }
-
-  // logout() {
-  //   this.auth.logout();
-  // }
 }
